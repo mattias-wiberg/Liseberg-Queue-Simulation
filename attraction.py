9df933213ad464bb521884f0cca1d1ec4ac8113b
alexeygorskiy <alexey.gorskiy@gmail.com>
2021-11-21 21:07:01 +0100

    think calc_queue_time is done

--- attraction.py
+++ attraction.py
@@ -1,5 +1,4 @@
 from wagon import Wagon
-import math
 
 class Attraction:
     __check_back_limit = 5    # how far back to check in the q to fill up odd spots
@@ -18,55 +17,91 @@ class Attraction:
         self.__wagons = [Wagon() for i in range(n_wagons)]
         
         self.__queue = []
-        self.__currently_riding = []
         self.__queue_time = 0
 
     def add_to_queue(self, agent):
         self.__queue.append(agent)
 
-    def remove_from_queue(self, idx=0):
-        return self.__queue.pop(idx)
-
     def advance_queue(self, global_time):
-        if global_time % self.__wagon_arrival_time == 0:
+        # this function modifies the internal variables
+        if (global_time % self.__wagon_arrival_time) == 0:
             # let people off
             current_wagon_idx = (global_time/self.__wagon_arrival_time) % self.__n_wagons
-            leaving_agent = current_wagon_idx.clear()
+            current_wagon = self.__wagons[current_wagon_idx]
+            leaving_agent = current_wagon.clear()
             # TODO: update the leaving agent's state
         else:
             # no arriving wagon so nothing to update
             return
 
         if len(self.__queue) == 0:
-            # q is empty
+            # queue is empty
             return 
         
         places_left = self.__wagon_size
-        while places_left > 0:
-            # fill up a wagon
+        next_in_line_size = self.__queue[0].get_group_size()
+        while places_left >= next_in_line_size:
+            # fill up the wagon
+            agent = self.__queue.pop(0)
+            places_left -= agent.get_group_size()
+            current_wagon.add_agent(agent)
+            # TODO: update agent state
+            
+            if len(self.__queue) == 0:
+                return
+            
             next_in_line_size = self.__queue[0].get_group_size()
             
-            if places_left > next_in_line_size:
-                # next in line has enough space on the ride, let them through
-                agent = self.remove_from_queue()
-                places_left -= next_in_line_size
-                current_wagon_idx.add_agent(agent)
-            else:
-                # next in line does not have enough space on the ride,
-                # check if there is anyone else behind in the q that can fill up the spots (up to self.__check_back_limit)
-                for i in range(1, self.__check_back_limit):
-                    if len(self.__queue) > i and self.__queue[i].get_group_size() < places_left:
-                        places_left -= self.__queue[i].get_group_size()
-                        self.remove_from_queue(i)
-                # if the people in the back can't get on, send the wagon away
-                places_left = 0
+        # check if anyone in the back can fit on the ride (up to self.__check_back_limit)
+        for i in range(1, self.__check_back_limit):
+            if len(self.__queue) > i and self.__queue[i].get_group_size() < places_left:
+                agent = self.__queue.pop(i)
+                places_left -= agent.get_group_size()
+                current_wagon.add_agent(agent)
+                # TODO: update agent state
 
 
-
-
-
-    
-
+    def __fake_advance_queue(self, fake_global_time, fake_queue):
+        # very similar to advance_queue but no internal variables are modified
+        if len(fake_queue) == 0 or (fake_global_time % self.__wagon_arrival_time != 0):
+            # queue is empty
+            return fake_queue
         
+        places_left = self.__wagon_size
+        next_in_line_size = fake_queue[0].get_group_size()
+        while places_left >= next_in_line_size:
+            # fill up the wagon
+            places_left -= next_in_line_size
+            fake_queue.pop()
+            
+            if len(fake_queue) == 0:
+                return fake_queue
+            
+            next_in_line_size = fake_queue[0].get_group_size()
+            
+        # check if anyone in the back can fit on the ride (up to self.__check_back_limit)
+        for i in range(1, self.__check_back_limit):
+            if len(fake_queue) > i and fake_queue[i].get_group_size() < places_left:
+                places_left -= fake_queue.pop(i).get_group_size()
+
+        return fake_queue
+
+
+    def calc_queue_time(self, global_time):
+        fake_queue = self.__queue.copy()
+        fake_global_time = global_time.copy()
+
+        # calculate nearest modulo time
+        if (fake_global_time % self.__wagon_arrival_time) == 0:
+            to_add = 0
+        else:
+            to_add = self.__wagon_arrival_time - (fake_global_time % self.__wagon_arrival_time)
+
+        fake_global_time += to_add
+
+        while len(fake_queue) > 0:
+            self.__fake_advance_queue(fake_global_time, fake_queue)
+            fake_global_time += self.__wagon_arrival_time
+
 
 
