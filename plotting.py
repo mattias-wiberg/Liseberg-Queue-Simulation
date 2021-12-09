f694731904346d28fb67858bc8924c25159e3a77
alexeygorskiy <alexey.gorskiy@gmail.com>
2021-12-09 11:55:29 +0100

    added plot_cum_queue_time_per_attraction

--- plotting.py
+++ plotting.py
@@ -1,22 +1,14 @@
 import pickle
 from statistics import Statistics
 
-#cumulative number of agents at every attraction at every time step and also the number of agents not any attraction,
-#average cum time in queue at every time step,
-
+# average cum time in queue at every time step,
 # histogram: changing with time for queue at every time period
+# agents sorted by queue time and plotted as a distribution?
 
 world = pickle.load(open("pickles/world.p", "rb"))
 stats = Statistics(world)
 #stats.plot_num_agents_per_attraction()
 #stats.plot_queue_time_per_attraction()
-stats.plot_agent_cum()
+#stats.plot_agent_cum()
+stats.plot_cum_queue_time_per_attraction()
 
-"""
-queue_time_histories = list(map(lambda attraction : attraction.get_queue_time_history(), attractions))
-for t, world_at_t in enumerate(world.get_history()):
-    agents = world_at_t[0]
-    attractions = world_at_t[1]
-
-    x = 5
-"""
--- statistics.py
+++ statistics.py
@@ -2,6 +2,21 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 class Statistics():
+    def __calc_queue_time_per_attraction(self):
+        queue_time_history = []
+        for time_step in range(len(self.time_values)):
+            attractions = self.history[time_step][1]
+            queue_time_history.append(list(map(lambda attraction:attraction.get_queue_time_history()[-1], attractions)))
+        return np.array(queue_time_history)
+
+    def __calc_num_agents_per_attraction(self):
+        num_agents_history = []
+        for time_step in range(len(self.time_values)):
+            attractions = self.history[time_step][1]
+            num_agents = list(map(lambda attraction:attraction.get_num_agents(), attractions))
+            num_agents_history.append(num_agents)
+        return np.array(num_agents_history)
+    
     def __init__(self, world):
         self.world = world
         self.attraction_names = ["Helix", "AtmosFear", "Lisebergsbanan", "Loke", "Balder", 
@@ -9,39 +24,55 @@ class Statistics():
         # pickle has trouble with non-ASCII characters, so that is why the names are here
         self.time_values = list(range(len(self.world.get_history())))
         self.history = self.world.get_history()
+        self.total_num_people = sum(list(map(lambda agent:agent.get_group_size(), self.history[0][0])))
 
-    def plot_queue_time_per_attraction(self):
-        # TODO: animated histo plot instead
-        queue_time_history = []
-        for time_step in range(len(self.time_values)):
-            attractions = self.history[time_step][1]
-            queue_time_history.append(list(map(lambda attraction:attraction.get_queue_time_history()[-1], attractions)))
-        queue_time_history = np.array(queue_time_history)
+        self.queue_time_history = self.__calc_queue_time_per_attraction()
+        self.num_agents_history = self.__calc_num_agents_per_attraction()
+
+    def plot_cum_queue_time_per_attraction(self):
+        # to get the real cumulative queue time, multiply the cum_queue_time array with the time_step size
+        # right now the unit is number of time steps waited in queue
+        cum_queue_time = np.zeros( (len(self.time_values), len(self.attraction_names)) )
+        cum_queue_time[0,:] = self.num_agents_history[0,:]
+        for time_step in range(1, len(self.time_values)):
+            cum_queue_time[time_step,:] = self.num_agents_history[time_step,:] + cum_queue_time[time_step-1,:]
+        cum_queue_time = np.array(cum_queue_time)
 
         plt.clf()
         for i, attraction_name in enumerate(self.attraction_names):
-            plt.plot(self.time_values, queue_time_history[:,i], label = attraction_name)
+            plt.plot(self.time_values, cum_queue_time[:,i], label = attraction_name)
 
-        plt.xlabel('Time Step [s]')
-        plt.ylabel('Queue Time [s]')
+        plt.xlabel('Time Step')
+        plt.ylabel('Cumulative Queue Time [number of timesteps]')
+        plt.title('Cumulative Queue Time Per Attraction')
+        plt.legend()
+        plt.show()
+
+
+    def plot_queue_time_per_attraction(self):
+        # TODO: animated histo plot instead
+        plt.clf()
+        for i, attraction_name in enumerate(self.attraction_names):
+            plt.plot(self.time_values, self.queue_time_history[:,i], label = attraction_name)
+
+        plt.xlabel('Time Step')
+        plt.ylabel('Queue Time [number of timesteps]')
         plt.title('Queue Time Per Attraction')
         plt.legend()
         plt.show()
     
     def plot_num_agents_per_attraction(self):
         # TODO: animated histo plot instead
-        num_agents_history = []
-        for time_step in range(len(self.time_values)):
-            attractions = self.history[time_step][1]
-            num_agents = list(map(lambda attraction:attraction.get_num_agents(), attractions))
-            num_agents_history.append(num_agents)
-        num_agents_history = np.array(num_agents_history)
+        num_agents_in_attractions = np.sum(self.num_agents_history, axis=1)
+        num_agents_in_park = np.array(self.total_num_people) - num_agents_in_attractions
 
         plt.clf()
         for i, attraction_name in enumerate(self.attraction_names):
-            plt.plot(self.time_values, num_agents_history[:,i], label = attraction_name)
+            plt.plot(self.time_values, self.num_agents_history[:,i], label = attraction_name)
+        
+        plt.plot(self.time_values, num_agents_in_park, label = "Not In Attraction")
 
-        plt.xlabel('Time Step [s]')
+        plt.xlabel('Time Step')
         plt.ylabel('Number of Agents')
         plt.title('Number of Agents Per Attraction')
         plt.legend()
@@ -70,7 +101,7 @@ class Statistics():
         for i, attraction_name in enumerate(self.attraction_names):
             plt.plot(self.time_values, agent_cum[:,i], label = attraction_name)
 
-        plt.xlabel('Time Step [s]')
+        plt.xlabel('Time Step')
         plt.ylabel('Cumulative Number of Agents')
         plt.title('Cumulative Number of Agents Per Attraction')
         plt.legend()
