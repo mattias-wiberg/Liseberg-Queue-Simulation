dc52766218b5cffd0daea336b6a563fa4a98d990
Mattias <klutten99@gmail.com>
2021-12-09 20:17:19 +0100

    New gif

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ random.seed(10)
 if __name__ == '__main__':
     #model = Model(mix=[(Type.RANDOM, 0.5),(Type.SMART, 0.5)])
 	delays = [0]#[0, 30, 60, 5*60, 10*60, 20*60, 30*60, 40*60, 60*60]
-	mix = [(Type.RANDOM, 1)]
+	mix = [(Type.SMART, 1)]
 	for delay in delays:
 		model = Model(mix,delay)
 
