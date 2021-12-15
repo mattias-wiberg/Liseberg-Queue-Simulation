b54fd003205975c82b10ca42134c26e8a02c9989
klutten99 <klutten99@gmail.com>
2021-12-15 12:57:20 +0100

    Update attractions_plot.py

--- misc/attractions_plot.py
+++ misc/attractions_plot.py
@@ -3,34 +3,44 @@ import matplotlib.pyplot as plt
 import matplotlib.animation
 
 names = []
+
+
 def load_data(path):
-    data = np.genfromtxt(path, delimiter=',', dtype=("U20",float,float,int,int,int))
-    
-    R = 6371000 # World radius
+    data = np.genfromtxt(path, delimiter=',', dtype=(
+        "U20", "U20", float, float, int, int, int))
+
+    R = 6371000  # World radius
+    colors = ['blue', 'black', 'red', 'green', 'orange',
+              'brown', 'violet', 'olive', 'indigo', 'tan', 'slategray']
     origin = data[0]
-    lat = origin[1]
-    lon = origin[2]
+    lat = origin[2]
+    lon = origin[3]
     org_x = R * np.cos(np.deg2rad(lat)) * np.cos(np.deg2rad(lon))
     org_y = R * np.cos(np.deg2rad(lat)) * np.sin(np.deg2rad(lon))
-
+    i = 0
     for row in data[0:]:
-        name = row[0]
-        lat = row[1]
-        lon = row[2]
+        name = row[1].replace("_", " ")
+        lat = row[2]
+        lon = row[3]
         x = R * np.cos(np.deg2rad(lat)) * np.cos(np.deg2rad(lon))
         y = R * np.cos(np.deg2rad(lat)) * np.sin(np.deg2rad(lon))
         names.append(name)
-        x,y = np.array([[0, 1],[-1, 0]]).dot([x-org_x,y-org_y])
-        plt.scatter(x, y)
+        x, y = np.array([[0, 1], [-1, 0]]).dot([x-org_x, y-org_y])
+
+        if row[0] == "SPAWN":
+            plt.scatter(x, y, marker='^', c=[[170/250, 0, 1]])
+        else:
+            plt.scatter(x, y, marker='s', c=colors[i])
+            i += 1
     plt.axis('equal')
     plt.grid(False)
     plt.legend(names)
     plt.show()
-        
 
-#load_data("park_data.csv")
+
+load_data("./data/park_data.csv")
 a = []
-visit = [1,2,3,4,5]
+visit = [1, 2, 3, 4, 5]
 print(a)
 for to_visit in visit:
     if len(a) != 3:
@@ -38,5 +48,5 @@ for to_visit in visit:
     else:
         a.remove(a[0])
         a.append(to_visit)
-    
+
     print(a)
